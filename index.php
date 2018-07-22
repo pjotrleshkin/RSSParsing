378c16e9d1c07aa9e9893906480f73bb0af973bc
MacMini <MacMini@Mac-mini.local>
2018-07-22 21:06:37 +0300

    Final commit
    changed POST to GET

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ foreach ($rss->channel->item as $item) {
 echo '<div class = "col-sm-4 bordered">';
 echo "<p  class = 'time underscored'>".$item->pubDate."</p>";
 //store link to resourse in data-link attribute
-echo "<a href='requests/mercury.php' data-link = ".$item->link." target='_blank'>";
+echo "<a href='requests/mercury.php?url={$item->link}' data-link = ".$item->link." target='_blank'>";
 //funky syntax, but retrieves the image from XML
 echo '<img class="card-img-top" src="'.$item[1]->children('media', True)->content->attributes().'" alt="Sorry, there is no picture">';
 echo '<h6 class="card-title">'.$item->title.'</h6>'.'</a>';
@@ -36,17 +36,15 @@ if ($item->author == true) {
 echo '</div>';
 }?>
 <script>
+
 $('a').click(function() {
-
-
-
-
+let adress = $(this).data('link');
     $.ajax({
       type: "POST",
       url: '/requests/mercury.php',
-      data: {url : $(this).data('link')},
+      data: {url : adress},
        success: function(data){
-           console.log(data);
+           console.log('success');
        }
    });
 
--- requests/mercury.php
+++ requests/mercury.php
@@ -1,16 +1,22 @@
 <?php
 $exampleLink = "https://trackchanges.postlight.com/building-awesome-cms-f034344d8ed";
 
-$link = $_POST['url'];
-var_dump($_POST['url']);
-echo "<br><br>";
+$link = "";
 
+if (isset($_POST['title'])) {
+    $link = $_POST['url'];
+} else  {
+    $link = $_GET['url'];
+}
 $ch = curl_init();
+
 curl_setopt($ch, CURLOPT_URL, "https://mercury.postlight.com/parser?url=".$link);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     "x-api-key: AU5zjyEqyL0LgR8zkb4Iurhlenpn52bGtbiqJA4t",
 ));
 
 $x = curl_exec($ch);
+
 curl_close($ch);
+
 ?>
